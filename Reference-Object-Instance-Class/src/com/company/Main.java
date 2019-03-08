365d9223bf64c99a0210e224176315e8dfa4a449
Bill Sheng <billxsheng@gmail.com>
2019-03-07 20:14:39 -0500

    reference-object-instance-class

---
+++ Reference-Object-Instance-Class/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        A CLASS is a blueprint for a house, using the blueprint we can build as many houses as we like based on those plans
+//        Each house you build is an OBJECT known as an INSTANCE
+//        Each house has an address (REFERENCE)
+//        You can copy that reference as many times but there is still only one house
+//        We can pass references as parameters to constructors and methods
+
+
+//        House blueHouse = new House("blue");
+//        House anotherHouse = blueHouse;
+//        This creates a reference to the same object in memory
+
+//        anotherHouse.setColor("yellow");
+//        both house references have the same color because both houses are pointing to the same object
+
+
+
+    }
+}
