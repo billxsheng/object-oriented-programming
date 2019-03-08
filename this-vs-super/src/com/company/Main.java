f2b4128e0def0e3c6aba9ff4f56b5d70cad3ac3d
Bill Sheng <billxsheng@gmail.com>
2019-03-07 20:21:37 -0500

    this-vs-super

---
+++ this-vs-super/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        super keyword is used to access/call the parent class members (variables and methods)
+
+//        this is used to call the current class members (variables and methods)
+//        required when we have a parameter that has the same name as the field
+
+//        note that we can use both of them anywhere in a class except static areas
+
+
+//        method overriding is when we call a method with the same name from the parent class
+
+//        this() is used with constructor chaining and must be the first line in the constructor
+//        super() calls the parent constructor also the first statement
+//        a call cannot call this() and super()
+
+    }
+}
