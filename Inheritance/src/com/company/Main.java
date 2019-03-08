007b8c56116699af164b074fdeca597803761a0c
Bill Sheng <billxsheng@gmail.com>
2019-03-07 19:29:29 -0500

    Inheritance

--- Inheritance/src/com/company/Main.java
+++ Inheritance/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+        Dog dog = new Dog("Yorkie", 8, 100, 4, 4, 10,  5, "Blonde");
+//        dog.eat();
+        dog.walk();
+//        dog.run();
     }
 }
