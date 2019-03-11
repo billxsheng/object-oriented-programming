7f1a04cb8304d7b58ec347c976174b99962834f9
Bill Sheng <billxsheng@gmail.com>
2019-03-10 20:15:19 -0400

    composition, encapsulation

--- Composition/src/com/company/Main.java
+++ Composition/src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    //Composition is a has-a relationship
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Resolution resolution = new Resolution(5, 5);
+        Case theCase = new Case("220B", "Apple", "240", dimensions);
+        Monitor monitor = new Monitor("418A", "Apple", 40, resolution);
+        Motherboard motherboard = new Motherboard("418C", "Apple", 5, 10, "idk");
+        PC myMac = new PC(theCase, monitor, motherboard);
+        myMac.powerUp();
     }
 }
--- Encapsulation/src/com/company/Main.java
+++ Encapsulation/src/com/company/Main.java
@@ -3,6 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    //mechanism which allows you to restrict access to certain components in the objects you create
+    //protects inner working from unwanted external access
+
+//wrong method
+
+//        Player bill = new Player();
+//        bill.name = "bill";
+//        bill.health = 20;
+//        bill.weapon = "sword";
+//
+//        int damage = 10;
+//        bill.loseHealth(damage);
+//        System.out.println("Remaining health = " + bill.healthRemaining());
+//
+//        damage = 11;
+//        bill.health = 200;
+//        bill.loseHealth(damage);
+//        System.out.println("Remaining health = " + bill.healthRemaining());
+
+//Mutable object is an object where the states and fields can be changed after the object is initialized (StringBuilder)
+//Immutable objects cannot be changed after it is created (String)
+
+        EnhancedPlayer player = new EnhancedPlayer("bill", 180, "Sword");
+        System.out.println(player.getHitPoints());
+
     }
 }
